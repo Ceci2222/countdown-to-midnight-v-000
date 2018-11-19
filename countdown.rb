d211577551a9131457c0fa85271b41c8e8d5a027
Marie Burns <mcburns2222@gmail.com>
2018-11-19 17:31:43 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
+def countdown_with_sleep(num_secs)
+  sleep(num_secs)
+end
+
+
 def countdown
+  count = 10
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    countdown_with_sleep(1)
+  end
+    puts "HAPPY NEW YEAR!"
 end
